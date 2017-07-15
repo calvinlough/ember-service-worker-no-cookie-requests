23a77f3e9d0977387b7beb4c585b0ed838993941
Calvin Lough <calvinlough@gmail.com>
2017-07-15 18:54:17 -0400

    Set up basic files

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* eslint-env node */
 'use strict';
 
+var Config = require('./lib/config');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-service-worker-no-cookie-requests'
+  name: 'ember-service-worker-no-cookie-requests',
+
+  included: function(app) {
+    this._super.included && this._super.included.apply(this, arguments);
+    this.app = app;
+    this.app.options = this.app.options || {};
+    this.app.options['esw-no-cookie-requests'] = this.app.options['esw-no-cookie-requests'] || {};
+  },
+
+  treeForServiceWorker(swTree, appTree) {
+    var options = this.app.options['esw-no-cookie-requests'];
+    var configFile = new Config([appTree], options);
+
+    return mergeTrees([swTree, configFile]);
+  }
 };

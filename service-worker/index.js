5622428a897b9ced99f21a3c8a5ede74dc9b2464
Calvin Lough <calvinlough@gmail.com>
2017-07-15 20:29:55 -0400

    Implement service worker logic

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,5 +1,17 @@
+import { PATTERNS } from 'ember-service-worker-no-cookie-requests/service-worker/config';
+import { createUrlRegEx, urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
+
+const PATTERN_REGEX = PATTERNS.map(createUrlRegEx);
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request.url, { credentials: 'omit' })
-  );
+  let request = event.request;
+  if (request.method !== 'GET' || !/^https?/.test(request.url)) {
+    return;
+  }
+
+  if (urlMatchesAnyPattern(request.url, PATTERN_REGEX)) {
+    event.respondWith(
+      fetch(event.request.url, { credentials: 'omit' })
+    );
+  }
 });
